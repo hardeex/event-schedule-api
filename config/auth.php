f2c876e27893ff07810d1110920ca399bf24e1d2
Adewale <webmasterjdd@gmail.com>
2025-08-01 05:40:15 +0100

    setup jwt

--- config/auth.php
+++ config/auth.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Authentication Defaults
@@ -39,6 +38,12 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],
+
+        'api' => [
+            'driver' => 'jwt', 
+            'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
@@ -111,5 +116,4 @@ return [
     */
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
-
 ];
